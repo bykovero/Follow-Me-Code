764e7fb9fc52a84c3a9596eeca19b487272999dd
desophie <129333231+desophie@users.noreply.github.com>
2023-05-11 13:03:14 +0200

    Create PICamera.py

---
+++ PICamera.py
@@ -0,0 +1,42 @@
+# Importing all modules
+import cv2
+import numpy as np
+
+# Specifying upper and lower ranges of color to detect in hsv format
+lower = np.array([25, 80, 50])
+upper = np.array([45, 255, 255]) # (These ranges will detect Yellow)
+
+#hue: 45° (warm yellow) - 90° (yellow green)
+#saturation: white (0) - colour (255)
+#value: black (0) - colour (255)
+
+# Capturing webcam footage
+webcam_video = cv2.VideoCapture(0)
+
+while True:
+    success, video = webcam_video.read() # Reading webcam footage
+
+    img = cv2.cvtColor(video, cv2.COLOR_BGR2HSV) # Converting BGR image to HSV format
+
+    mask = cv2.inRange(img, lower, upper) # Masking the image to find our color
+
+    mask_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # Finding contours in mask image
+
+    # Finding position of all contours
+    if len(mask_contours) != 0:
+        for mask_contour in mask_contours:
+            if cv2.contourArea(mask_contour) > 500:
+                x, y, w, h = cv2.boundingRect(mask_contour)
+                cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+
+    cv2.imshow("mask image", mask) # Displaying mask image
+
+    cv2.imshow("window", video) # Displaying webcam image
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    
+webcam_video.release()
+
+cv2.destroyAllWindows()
