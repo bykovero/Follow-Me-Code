eb8a51eee1fbef3b0a24e7e800335aa89578fcce
desophie <129333231+desophie@users.noreply.github.com>
2023-05-11 13:16:39 +0200

    PICamera.py

--- PICamera.py
+++ PICamera.py
@@ -1,7 +1,14 @@
 # Importing all modules
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import cv2
 import numpy as np
 
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+raw.Capture = PiRGBArray(camera, size =(640, 480))
+                    
 # Specifying upper and lower ranges of color to detect in hsv format
 lower = np.array([25, 80, 50])
 upper = np.array([45, 255, 255]) # (These ranges will detect Yellow)
@@ -11,12 +18,14 @@ upper = np.array([45, 255, 255]) # (These ranges will detect Yellow)
 #value: black (0) - colour (255)
 
 # Capturing webcam footage
-webcam_video = cv2.VideoCapture(0)
+from frame in camera.capture_continuous(raw.Capture, format = "bgr", use_video_port=True):
+    image = frame.array
+    
 
-while True:
-    success, video = webcam_video.read() # Reading webcam footage
+#while True:
+    #success, video = webcam_video.read() # Reading webcam footage
 
-    img = cv2.cvtColor(video, cv2.COLOR_BGR2HSV) # Converting BGR image to HSV format
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Converting BGR image to HSV format
 
     mask = cv2.inRange(img, lower, upper) # Masking the image to find our color
 
@@ -27,16 +36,15 @@ while True:
         for mask_contour in mask_contours:
             if cv2.contourArea(mask_contour) > 500:
                 x, y, w, h = cv2.boundingRect(mask_contour)
-                cv2.rectangle(video, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 3) #drawing rectangle
 
     cv2.imshow("mask image", mask) # Displaying mask image
 
-    cv2.imshow("window", video) # Displaying webcam image
-
+    cv2.imshow("window", image) # Displaying webcam image
+    
+    rawCapture.truncate(0)
+    
     if cv2.waitKey(1) == ord('q'):
         break
-
-    
-webcam_video.release()
 
 cv2.destroyAllWindows()
