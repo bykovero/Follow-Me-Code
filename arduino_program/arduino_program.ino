0d86d852a875d1f78c2b360661cc1c227654d783
desophie <129333231+desophie@users.noreply.github.com>
2023-05-25 14:04:01 +0200

    Update arduino_program.ino

--- arduino_program/arduino_program.ino
+++ arduino_program/arduino_program.ino
@@ -1,14 +1,18 @@
 //PIN BELEGUNG
-int left_forward = 9
-int left_backward = 8
-int right_forward = 6
-int right_backward = 7
+int left_forward = 9;
+int left_backward = 8;
+int right_forward = 6;
+int right_backward = 7;
 
-int left_speed = 10
-int right_speed = 11
+int left_speed = 10;
+int right_speed = 11;
 
-int max_speed = 100
-int min_speed = 50
+//SPEED VARIABLEN
+int max_speed = 100;
+int min_speed = 50;
+
+int turn_speed_aussen = 70;
+int turn_speed_innen = 50;
 
 void setup() {
   Serial.begin(9600);
@@ -24,7 +28,7 @@ void setup() {
 
 }
 
-void drive_forwards(speed) {
+void drive_forwards(int speed) {
   Serial.println("Driving forwards!");
   analogWrite(left_speed, speed);
   analogWrite(right_speed, speed);
@@ -37,20 +41,39 @@ void drive_forwards(speed) {
   delay(1000);
 }
 
-void drive_right(speed, degree){
+void turn_right(int speed_aussen, int speed_innen){
   Serial.println("Driving Right!");
 
   //Calculate indivudual speeds based on degree
 
   //Drive
+  analogWrite(left_speed, speed_aussen);
+  analogWrite(right_speed, speed_innen);
 
+  digitalWrite(left_forward, HIGH);
+  digitalWrite(left_backward, LOW);
+  digitalWrite(right_forward, HIGH);
+  digitalWrite(right_backward, LOW);
 }
 
-void drive_left(speed, degree){
+void turn_left(int speed_aussen, int speed_innen){
   Serial.println("Driving Left!");
+  analogWrite(left_speed, speed_innen);
+  analogWrite(right_speed, speed_aussen);
+
+  digitalWrite(left_forward, HIGH);
+  digitalWrite(left_backward, LOW);
+  digitalWrite(right_forward, HIGH);
+  digitalWrite(right_backward, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("I am working!");
+  //Serial.println("I am working!");
+  //Serial.println("Hallo!");
+
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.println(data);
+  }
 }
